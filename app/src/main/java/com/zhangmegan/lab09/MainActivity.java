f3a9958d5bd82075ef14da993cd3f6bf444d36b3
mmz7 <mmzhang711@gmail.com>
2021-12-15 14:14:29 -0500

    hold button action repeats

--- app/src/main/java/com/zhangmegan/lab09/MainActivity.java
+++ app/src/main/java/com/zhangmegan/lab09/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     Button up, down, left, right;
     boolean flag = true;
     boolean collide = false;
-    View.OnClickListener listener;
+    View.OnTouchListener listener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,20 +24,6 @@ public class MainActivity extends AppCompatActivity {
         down = findViewById(R.id.down);
         left = findViewById(R.id.left);
         right = findViewById(R.id.right);
-
-//        up.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Sprite s = drawView.sprite;
-//                if(s.getdY() != 0)
-//                    s.setdY(-Math.abs(s.getdY()));
-//                else {
-//                    s.setdY(-Math.abs(s.getdX()));
-//                    s.setdX(0);
-//                }
-//                s.update();
-//            }
-//        });
 
 //        up.setOnTouchListener(new View.OnTouchListener() {
 //            @Override
@@ -56,12 +43,64 @@ public class MainActivity extends AppCompatActivity {
 //                return false;
 //            }
 //        });
-        listener = new View.OnClickListener() {
+//        listener = new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View view) {
+//                Sprite s = drawView.sprite;
+//                if(view.getId() == R.id.buttonup) {
+//                    if(s.getdY() != 0)
+//                        s.setdY(-Math.abs(s.getdY()));
+//                    else {
+//                        s.setdY(-Math.abs(s.getdX()));
+//                        s.setdX(0);
+//                    }
+//                }
+//                if(view.getId() == R.id.down) {
+//                    if(s.getdY() != 0) {
+//                        s.setdY(Math.abs(s.getdY()));
+//                    }
+//                    else {
+//                        s.setdY(Math.abs(s.getdX()));
+//                        s.setdX(0);
+//                    }
+//                }
+//                if(view.getId() == R.id.right) {
+//                    if(s.getdX() != 0)
+//                        s.setdX(Math.abs(s.getdX()));
+//                    else {
+//                        s.setdX(Math.abs(s.getdY()));
+//                        s.setdY(0);
+//                    }
+//                }
+//                if(view.getId() == R.id.left) {
+//                    if(s.getdX() != 0)
+//                        s.setdX(-Math.abs(s.getdX()));
+//                    else {
+//                        s.setdX(-Math.abs(s.getdY()));
+//                        s.setdY(0);
+//                    }
+//                }
+//                if(!s.intersect(drawView.lWall) && !s.intersect(drawView.rWall) &&
+//                    !s.intersect(drawView.tWall) && !s.intersect(drawView.bWall)) {
+//                    s.update();
+//                }
+//                else
+//                    collide = true;
+//                if(collide) {
+//                    s.offset(-s.getdX(), -s.getdY());
+//                    collide = false;
+//                }
+//            }
+//        };
 
-            @Override
-            public void onClick(View view) {
-                Sprite s = drawView.sprite;
-                if(view.getId() == R.id.buttonup) {
+        listener = new View.OnTouchListener() {
+            Sprite s = drawView.sprite;
+            private Handler handler;
+            @Override public boolean onTouch(View v, MotionEvent motionEvent) {
+                s.setdY(-10);
+                s.setdX(0);
+                if(v.getId() == R.id.buttonup) {
                     if(s.getdY() != 0)
                         s.setdY(-Math.abs(s.getdY()));
                     else {
@@ -69,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                         s.setdX(0);
                     }
                 }
-                if(view.getId() == R.id.down) {
+                if(v.getId() == R.id.down) {
                     if(s.getdY() != 0) {
                         s.setdY(Math.abs(s.getdY()));
                     }
@@ -78,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
                         s.setdX(0);
                     }
                 }
-                if(view.getId() == R.id.right) {
+                if(v.getId() == R.id.right) {
                     if(s.getdX() != 0)
                         s.setdX(Math.abs(s.getdX()));
                     else {
@@ -86,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
                         s.setdY(0);
                     }
                 }
-                if(view.getId() == R.id.left) {
+                if(v.getId() == R.id.left) {
                     if(s.getdX() != 0)
                         s.setdX(-Math.abs(s.getdX()));
                     else {
@@ -94,22 +133,34 @@ public class MainActivity extends AppCompatActivity {
                         s.setdY(0);
                     }
                 }
-                if(!s.intersect(drawView.lWall) && !s.intersect(drawView.rWall) &&
-                    !s.intersect(drawView.tWall) && !s.intersect(drawView.bWall)) {
-                    s.update();
-                }
-//                else
-//                    collide = true;
-//                if(collide) {
-//                    s.offset(-s.getdX(), -s.getdY());
-//                    collide = false;
-//                }
+               if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                   if (handler != null)
+                       return true;
+                   handler = new Handler();
+                   handler.postDelayed(runnable, 100);
+               }
+               else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                   if(handler == null)
+                       return true;
+                   handler.removeCallbacks(runnable);
+                   handler = null;
+               }
+               return false;
             }
+
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    drawView.sprite.update();
+                    handler.postDelayed(this, 100);
+                }
+            };
+
         };
 
-        up.setOnClickListener(listener);
-        down.setOnClickListener(listener);
-        left.setOnClickListener(listener);
-        right.setOnClickListener(listener);
+        up.setOnTouchListener(listener);
+        down.setOnTouchListener(listener);
+        left.setOnTouchListener(listener);
+        right.setOnTouchListener(listener);
     }
 }
